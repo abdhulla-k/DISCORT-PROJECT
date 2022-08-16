35953d1870fd0b1932a8311798e12c41532e86a6
abdhulla-k <abdhullak149@gmail.com>
2022-08-16 15:39:55 +0530

    completed two link condent  from side bar

--- index.js
+++ index.js
@@ -3,6 +3,24 @@ function openNav() {
 }
 
 function closeNav() {
+    document.getElementById("mode-academy").style.width = "0px";
+    document.getElementById("swafty").style.width = "0px"; // close swafty page first
     document.getElementById("mySidenav").style.width = "0";
     document.body.style.backgroundColor = "white";
+}
+
+function safty() {
+    document.getElementById("swafty").style.width = "250px";
+}
+
+function saftyClose() {
+    document.getElementById("swafty").style.width = "0px";
+}
+
+function academy() {
+    document.getElementById("mode-academy").style.width = "250px";
+}
+
+function academyClose() {
+    document.getElementById("mode-academy").style.width = "0px";
 }
